bfb761533caa9e0babb96300903c938df175477b
Benjamin <9423435+benbot16@users.noreply.github.com>
2025-01-31 12:18:38 -0800

    Adds flightcon stuff to threaded

--- threaded_flightcon.py
+++ threaded_flightcon.py
@@ -6,13 +6,16 @@ from board import *
 from adafruit_bus_device.i2c_device import I2CDevice
 import adafruit_mpl3115a2
 from adafruit_lsm6ds.ism330dhcx import ISM330DHCX
+from adafruit_mcp9600 import MCP9600
 from datetime import datetime
-import espeak
+from espeakng import ESpeakNG
 
 # Required arming altitude - we need to get this high to allow apogee sensing to work
 required_alt = 1000
 # Pressure is in hPa
-sea_pressure = 102.3
+ground_pressure = 962
+# Alt difference to use when we've stopped "falling" during landing
+ALT_DELTA_LANDED = 2
 
 #Final variables to transmit to ground
 max_x_accel = 0
@@ -26,15 +29,32 @@ max_alt = 0
 start_alt = 0
 descent_rate = 10
 alt_values = []
-flight_finished = false
+flight_finished = False
 #Temperature
 max_temp = 0
 temp_values = []
+max_cockpit_temp = 0
+cockpit_temp_values = []
+max_ext_temp = 0
+ext_temp_values = []
 
-update_interval = 0.5 #in seconds
+#Init our devices
+accelerometerid = 0x6a
+altimeterid = 0x60
+cockpittempid = 0x67
+nosetempid = 0x66
+i2c = busio.I2C(SCL, SDA)
+altimeter = adafruit_mpl3115a2.MPL3115A2(i2c, address=altimeterid)
+accelerometer = ISM330DHCX(i2c, address=accelerometerid)
+cockpit_thermo = MCP9600(i2c, address=cockpittempid)
+nose_thermo = MCP9600(i2c, address=nosetempid)
+transmitter = ESpeakNG()
+
+
+#Set update interval (in seconds)
+update_interval = 0.5
 
 def xmit_string(instring):
-    transmitter = espeak.ESpeak()
     transmitter.say(instring)
     return
 
@@ -54,7 +74,7 @@ def accel_monitor_thread():
             max_y_accel = y
         if(z > max_z_accel):
             max_z_accel = z
-        time.sleep(update_interval)
+        time.sleep(update_interval * 0.1) # Updates faster due to acceleration
 
 def alt_monitor_thread():
     global max_alt, start_alt, flight_finished
@@ -70,8 +90,6 @@ def alt_monitor_thread():
         else: 
             if(curr_alt > required_alt):
                 apogee = True
-            else:
-                continue
         alt_values.append(curr_alt)
         time.sleep(update_interval)
     
@@ -85,5 +103,19 @@ def alt_monitor_thread():
             timeout += 1
         time.sleep(update_interval)
 
+    # Handles triggering landing
+    flight_finished = true
+
 def temp_monitor_thread():
     global max_temp, temp_values
+    while(not flight_finished):
+        cur_temp = altimeter.temperature
+        cur_temp_cockpit = cockpit_thermo.temperature
+        cur_temp_ext = nose_thermo.temperature
+        
+        temp_values.append(cur_temp)
+        cockpit_temp_values.append(cur_temp_cockpit)
+        ext_temp_values.append(cur_temp_ext)
+        time.sleep(update_interval)
+
+
